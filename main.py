71042ac01c7c52d5c2961236987bd68f3ccbbe73
sarg30 <girotrasarthak16@gmail.com>
2022-04-16 16:21:00 +0530

    pipelining implementation for R type instructions for data forwarding enabled

--- main.py
+++ main.py
@@ -41,7 +41,12 @@ def insertdatatomemory(datasection):
                     mem=mem+1
     
 
-# functions related to piplining implementation for data forwarding disabled
+
+# check for updation of inst_counter in the functions
+
+
+
+# functions related to pipelining implementation for data forwarding disabled
 
 
 def dfne_R_type(rd,rs1,rs2):
@@ -147,6 +152,43 @@ def dfne_sw(rd,rs2):
 
 
 
+#fucntions related to pipelining implementation for data forwarding enabled
+
+
+def dfe_R_type(rd,rs1,rs2):
+    """
+    implements the pipeline stages corresponding to 
+    R type instructions when data forwarding is disabled
+    """
+    global cc_df_enabled
+    global inst_counter
+
+    #searching for which clock cycle to impelement IF stage
+    i= int(cc_df_enabled+1)
+    while is_stall_dfe[i]==1:
+        i=i+1
+    df_enabled[inst_counter][i]="IF"
+    i=i+1
+
+    #searching for the clock cycle to implement the ID/RF stage
+    while is_stall_dfe[i]:
+        i=i+1
+    df_enabled[inst_counter][i]="ID/RF"
+    i=i+1
+
+    #checking for dependencies and stalls and then impelmenting the EXE,MEM,WB stages
+    while is_stall_dfe[i] or last_mem[Register_index[rs1]]>=i or last_mem[Register_index[rs2]]>=i:
+        df_enabled[inst_counter][i]="STALL"
+        is_stall_dfe[i]=1
+        i=i+1
+    df_enabled[inst_counter][i]="EXE"
+    df_enabled[inst_counter][i+1]="MEM"
+    df_enabled[inst_counter][i+2]="WB"
+
+    #updating the last MEM clock cycle for the destination register
+    last_mem[Register_index[rd]]=i+1
+
+
 
 #Functions related to processing different types of instructions and returning required registers, labels and constants
 
@@ -156,10 +198,8 @@ def process_R_type(line):
     the memory addresses of 
     the registers for R-type instructions
     """
-    global inst_counter
     print("asdf1")
     dfne_R_type(line[1],line[2],line[3])
-    inst_counter=inst_counter+1
     return line[1], line[2], line[3]
 
 def process_I_type(line):
@@ -187,22 +227,28 @@ def process_J_type(line):
 #Functions related to processing load and save instructions
 def la(line):
     global pc
+    global inst_counter
     rd, rs2 = line[1], line[2]
+    inst_counter=inst_counter+1
     index = memdict[rs2+":"]
     RegisterVals[Register_index[rd]]=index*4
     pc=pc+1
 
 def li(line):
     global pc
+    global inst_counter
     rd, rs2 = line[1], line[2]
     RegisterVals[Register_index[rd]]=int(rs2)
+    inst_counter=inst_counter+1
     pc=pc+1
 
 def lw(line):
     global pc
     # lw rd, offset_12(base)
     rd, rs2 = line[1], line[2]
+    global inst_counter
     dfne_lw(rd,rs2)
+    inst_counter=inst_counter+1
     linelist = [x.strip() for x in rs2.split('(')]
     offset = linelist[0]
     linelist = [x.strip() for x in linelist[1].split(')')]
@@ -216,6 +262,9 @@ def sw(line):
     global pc
     #sw rd, offset_12(base)
     rd,rs2=line[1],line[2]
+    global inst_counter
+    dfne_sw(rd,rs2)
+    inst_counter=inst_counter+1
     linelist = [x.strip() for x in rs2.split('(')]
     offset = linelist[0]
     linelist = [x.strip() for x in linelist[1].split(')')]
@@ -229,7 +278,9 @@ def sw(line):
 
 def beq(line):
     global pc
+    global inst_counter
     rs1, rs2, nextaddress = process_B_type(line) 
+    inst_counter=inst_counter+1
     if RegisterVals[Register_index[rs1]] == RegisterVals[Register_index[rs2]]:
         pc = jumpdict[nextaddress+":"]
     else:
@@ -238,7 +289,9 @@ def beq(line):
 
 def bne(line):
     global pc
+    global inst_counter
     rs1, rs2, nextaddress = process_B_type(line)
+    inst_counter=inst_counter+1
     if RegisterVals[Register_index[rs1]] != RegisterVals[Register_index[rs2]] :
         pc = jumpdict[nextaddress+":"]
     else:
@@ -246,7 +299,9 @@ def bne(line):
 
 def ble(line):
     global pc
+    global inst_counter
     rs1, rs2, nextaddress = process_B_type(line)
+    inst_counter=inst_counter+1
     #print(RegisterVals[Register_index[rs1]])
     #print(RegisterVals[Register_index[rs2]])
     print(memory[0:mem])
